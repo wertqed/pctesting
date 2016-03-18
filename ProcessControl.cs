7587d5cd8df00c63e7d5bc3066fe8269c4efb354
wertqed <belo.199666@mail.ru>
2016-03-18 16:25:23 +0300

    Класс где собирается информация по процессам

---
+++ ProcessControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace pctetsting
+{
+    class ProcessControl
+    {
+        Process[] proc;
+
+        public void UpdateProcess()
+        {
+            proc = Process.GetProcesses();
+        }
+    }
+}
