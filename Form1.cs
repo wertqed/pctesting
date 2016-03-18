79ccea4263d4efc99906135ceffcc01701310367
wertqed <belo.199666@mail.ru>
2016-03-18 16:33:55 +0300

    Я сделал, чтобы в трей закидывало

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pctetsting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            if(this.WindowState==FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        ProcessControl process = new ProcessControl();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            process.UpdateProcess();
+        }
+    }
+}
